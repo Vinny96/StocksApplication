1e9f4d194f4b522f5795a60ab32e4efcb0166ff9
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-10-25 14:03:41 -0400

    Removed comments

--- StocksApp/StocksApp/Models/TopNews.swift
+++ StocksApp/StocksApp/Models/TopNews.swift
@@ -8,18 +8,7 @@
 import Foundation
 
 struct TopNews : Codable
-{
-    /**
-     "category": "top news",
-     "datetime": 1635106560,
-     "headline": "Earnings Watch: Holiday earnings forecasts have been lumps of coal so far, and here come the biggest ones",
-     "id": 6972528,
-     "image": "https://images.mktw.net/im-418199/social",
-     "related": "",
-     "source": "MarketWatch",
-     "summary": "Holiday earnings forecasts are disappointing investors so far, and the biggest boxes under Wall Street's tree are coming this week.",
-     "url": "https://www.marketwatch.com/story/holiday-earnings-forecasts-have-been-lumps-of-coal-so-far-and-here-come-the-biggest-ones-11635106579"
-     */
+{T
     var category : String
     var datetime : TimeInterval
     var headline : String
