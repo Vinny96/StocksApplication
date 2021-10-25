5c85c23f81fff82c937e2701d6f1c94fa46b9642
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-10-25 17:59:53 -0400

    Initial commit for NewsStoryTableViewCell and NewsStoryViewModel

---
+++ StocksApp/StocksApp/View Models/NewsStoryViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  NewsStoryVIewModel.swift
+//  StocksApp
+//
+//  Created by Vinojen Gengatharan on 2021-10-25.
+//
+
+import Foundation
+import UIKit
+
+struct NewsStoryViewModel
+{
+    
+    
+    
+}
---
+++ StocksApp/StocksApp/Views/NewsStoryTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsStoryTableViewCell.swift
+//  StocksApp
+//
+//  Created by Vinojen Gengatharan on 2021-10-25.
+//
+
+import UIKit
+
+class NewsStoryTableViewCell: UITableViewCell {
+
+   
+
+}
